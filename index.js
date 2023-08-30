1b539fcda1fef8e93d5594e9ab057faeb0043995
acal-rafael <rafaelsv.corp@gmail.com>
2023-08-30 14:28:18 -0300

    feat: add config

--- index.js
+++ index.js
@@ -1,43 +1,74 @@
-
+/*
+*   primary file for the API
+*
+*/
 
 // Dependencies
 var http = require("http");
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
+const fs = require('fs');
 
-// The server should respond to all request with a string
-var server = http.createServer(function(req, res){
-    
+// Instantiate the HTTP server
+var httpServer = http.createServer(function(req, res){
+    unifiedServer(req, res);
+});
+
+// Start the HTTP server
+httpServer.listen(config.httpPort, function() {
+    console.log(`The server is listening on port ${config.httpPort}`)
+})
+
+// Instantiate the HTTPS server
+var httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem')
+};
+
+var httpsServer = https.createServer(httpsServerOptions, function(req, res){
+    unifiedServer(req, res);
+});
+
+// // Start the HTTPS server
+httpsServer.listen(config.httpsPort, function() {
+    console.log(`The server is listening on port ${config.httpsPort}`)
+})
+
+
+// Allthe server logic for both the http and https server
+var unifiedServer = function(req, res) {
     // Get the URL and parse it
     var parsedUrl = url.parse(req.url, true);
-
+    
     // Get the path
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
+    
     // Get the query string as an object
     var queryStringObject = parsedUrl.query;
 
     // Get the HTTP Method
     var method = req.method.toLowerCase();
-
+    
     // Get the headers as an object
     var headers = req.headers;
-
+    
     // Get the payload, if any
     var decoder = new StringDecoder('utf-8');
     var buffer = '';
-
+    
     req.on('data', function(data) {
         buffer += decoder.write(data);
     });
-
+    
     req.on('end', function() {
         buffer += decoder.end();
-
+        
         // Choose the handler this request should go to. If is not found, use the notFound handler
         var chooseHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
+        
         // Construct the data object to send to the handler
         var data = {
             'trimmedPath': trimmedPath,
@@ -57,37 +88,30 @@ var server = http.createServer(function(req, res){
 
             // Covert the payload to a string
             var payloadString = JSON.stringify(payload);
-
-
+            
+            
             // return the response
             res.setHeader('Content-Type', 'application/json');
             res.writeHead(statusCode);
             res.end(payloadString);
-
+            
             console.log('Returning this response: ', statusCode, payloadString);
         });
-
+        
         // Send the response       
         // Log the request path
         // console.log("Request received on path: "+ trimmedPath + " with method: "+ method + " and with these query string parameters ",  queryStringObject);
         // console.log('Request received with these headers: ', headers);
     });
-});
-
-// Start the server, and have it listen on port 3000
-server.listen(3000, function() {
-    console.log("The server is listening on port 3000 now")
-})
+}
 
 // Define the handlers
 var handlers = {}
 
-
-// Sample handler
-handlers.sample = function(data, callback) {
-    // Callback a http status code, and a payload object
-    callback(406, {'name': 'sample handler'});
-};
+// Ping handler
+handlers.ping = function(data, callback) {
+    callback(200);
+}
 
 // Not found handler
 handlers.notFound = function(data, callback) {
@@ -96,5 +120,5 @@ handlers.notFound = function(data, callback) {
 
 // Define a request router
 var router = {
-    'sample': handlers.sample
+    'ping': handlers.ping
 }
